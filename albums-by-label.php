e453df40f88ba38809f3606ee7e976c0c4d9c2d6
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 06:35:38 -0600

    Update albums-by-label.php

--- albums-by-label.php
+++ albums-by-label.php
@@ -18,7 +18,7 @@ if (isset($_POST['actionType'])) {
 }
 
 
-$albums = selectAlbumsByLabel($_POST['aid']);
+$albums = selectAlbumsByLabel($_POST['cid']);
 include "view-albums-by-label.php";
 include "view-footer.php";
 ?>
