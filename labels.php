05d4a089289c3f02424bbca9c4f0fcf74dc242a2
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 03:51:27 -0600

    Create labels.php

---
+++ labels.php
@@ -0,0 +1,37 @@
+<?php
+require_once("connection-db.php");
+require_once("model-labels.php");
+
+$pageTitle = "Labels";
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertLabel($_POST['cName'], $_POST['cYear'])) {
+        echo '<div class="alert alert-success" role="alert">Label added!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  case "Edit":
+      if (updateLabel($_POST['cName'], $_POST['cYear'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Label edited!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  case "Delete":
+      if (deleteLabel($_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Label deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+$labels = selectLabels();
+include "view-labels.php";
+include "view-footer.php";
+?>
