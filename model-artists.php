4014cea136a6d21e9d9322bea13da7158e5e9cc7
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 04:15:51 -0600

    Update model-artists.php

--- model-artists.php
+++ model-artists.php
@@ -13,4 +13,49 @@ function selectArtists() {
         throw $e;
     }
 }
+
+
+function insertArtists($aAName, $aGenre) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `artist` (`artist_name`, `artist_genre`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $aAName, $aGenre);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateArtists($aAName, $aGenre, $aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `artist` set `artist_name` = ?, `artist_genre` = ? where artist_id = ?");
+        $stmt->bind_param("ssi", $aAName, $aGenre, $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteArtists($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from artist where artist_id=?");
+        $stmt->bind_param("i", $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
