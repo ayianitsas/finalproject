08caf55eca3dfc7d715ae6e65811b9248232f1b7
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 07:46:01 -0600

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -1,44 +1,34 @@
-<div class="row">
-  <div class="col">
-    <h1>Artists with Labels</h1>
-  </div>
-</div>
+<h1>Artists</h1>
+<div class="table-responsive">
+  <table class="table">
 
-<table class="table table-striped">
+    <table style="background-color: #B17D85; color: black; border-collapse: collapse; width: 100%;">
+
   <thead>
     <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Genre</th>
-      <th>Click here to view labels!</th>
-      <th>Actions</th>
+    <th><strong>ID</strong></th>
+    <th><strong>Name</strong></th>
+    <th><strong>Genre</strong></th>
+
+
+      
     </tr>
   </thead>
-  <tbody>
-    <?php while ($artist = $artists->fetch_assoc()) { ?>
-      <tr id="row-<?php echo $artist['artist_id']; ?>">
-        <td><?php echo $artist['artist_id']; ?></td>
-        <td><?php echo $artist['artist_name']; ?></td>
-        <td><?php echo $artist['artist_genre']; ?></td>
-        <td>
-          <div id="labels-<?php echo $artist['artist_id']; ?>" class="labels-section">
-            <ul>
-              <?php
-              $labels = selectLabelsForArtists($artist['artist_id']);
-              while ($label = $labels->fetch_assoc()) { ?>
-                <li><?php echo $label['label_name']; ?> - <?php echo $label['label_yearestablished']; ?></li>
-              <?php } ?>
-            </ul>
-          </div>
-        </td>
-        <td>
-          <button class="btn btn-primary" type="button" 
-            onclick="document.getElementById('labels-<?php echo $artist['artist_id']; ?>').style.display='none'">
-            Hide
-          </button>
-        </td>
-      </tr>
-    <?php } ?>
-  </tbody>
-</table>
-
+    <tbody>
+          <table style="background-color: #B17D85; color: black; width: 100%; border-collapse: collapse;">
+<?php
+while ($artist = $artists->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $artist['artist_id']; ?></td>
+    <td><?php echo $artist['artist_name']; ?></td>
+    <td><?php echo $artist['artist_genre']; ?></td>
+    <td><a href="labels-for-artists.php?id=<?php echo $artist['artist_id']; ?>">Labels</a></td>
+  </tr>
+<?php
+}
+?>
+      
+    </tbody>
+  </table>
+</div>
