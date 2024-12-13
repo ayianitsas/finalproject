67f03aa547a59598ce58413bf19040c35f2ea7c6
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 04:45:32 -0600

    Update albums-by-label.php

--- albums-by-label.php
+++ albums-by-label.php
@@ -4,6 +4,19 @@ require_once("model-albums-by-label.php");
 
 $pageTitle = "Albums by Label";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertAlbum($_POST['title'], $_POST['year'])) {
+        echo '<div class="alert alert-success" role="alert">Album added!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  case "Edit":
+
+
 $albums = selectAlbumsByLabel($_POST['cid']);
 include "view-albums-by-label.php";
 include "view-footer.php";
