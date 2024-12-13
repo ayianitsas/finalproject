4166b96af4165a47b0188319edfb41d838b61411
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 04:51:12 -0600

    Update view-albums-by-label.php

--- view-albums-by-label.php
+++ view-albums-by-label.php
@@ -1,4 +1,13 @@
+<div class="row">
+  <div class="col">
 <h1><strong>Albums by Label</strong></h1>
+      </div>
+  <div class="col-auto">
+<?php
+include "view-albums-by-label-newform.php";
+?>
+ </div>
+</div>
 <div class="table-responsive">
   <table style="width:100%; border-collapse: collapse;">
     <thead style="background: linear-gradient(to right, #e91e63, #2196F3); color: black;">
@@ -8,9 +17,13 @@
       <th><b>Year Established</b></th>
       <th><b>Album Title</b></th>
       <th><b>Year</b></th>
+      <th></th>
+      <th></th>
+      <th></th>
       </tr>
     </thead>
     <tbody style="background-color: #484343; color: black;">
+     
 
 <?php
 while ($album = $albums->fetch_assoc()) {
