a003fb0c27f0bc13e94846f9c0f6c8605f7ef1fd
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 08:49:35 -0600

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -40,21 +40,14 @@ include "view-artists-newform.php";
           <td>
 
 
-
-    
-
-      
-
-               <td id="hidelabels">
-          <div id="jsfunction" class="section">
-            <button class="btn btn-primary" type="button" onclick="document.getElementById('hidelabels').style.display='none'">Hide</button>
-          </div>    
-
+             <div class="section">
+              <button class="btn btn-primary" type="button" onclick="document.getElementById('row-<?php echo $artist['artist_id']; ?>').style.display='none'">Hide
+              </button>
+            </div>
+          </td>
+          <td>
 
         
-       
-
-
       <?php
       include "view-artists-editform.php";
       ?>
