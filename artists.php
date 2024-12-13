3ded6d4b891669cebd0e7ac1cd0627502ad6654d
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 04:11:08 -0600

    Update artists.php

--- artists.php
+++ artists.php
@@ -4,6 +4,34 @@ require_once("model-artists.php");
 
 $pageTitle = "Artists";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertArtist($_POST['cName'], $_POST['cYear'])) {
+        echo '<div class="alert alert-success" role="alert">Artist added!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  case "Edit":
+      if (updateArtist($_POST['cName'], $_POST['cYear'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Artist edited!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  case "Delete":
+      if (deleteArtist($_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Artist deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+
 $artists = selectArtists();
 include "view-artists.php";
 include "view-footer.php";
