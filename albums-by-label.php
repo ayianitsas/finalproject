92be714d92ae6fbe3e2025b6d7dbe38d290fce2d
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 04:54:53 -0600

    Update albums-by-label.php

--- albums-by-label.php
+++ albums-by-label.php
@@ -14,7 +14,8 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
-  case "Edit":
+  }
+}
 
 
 $albums = selectAlbumsByLabel($_POST['cid']);
