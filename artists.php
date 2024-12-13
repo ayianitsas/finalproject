71a24328ceb61250be717492628381239c58b63c
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 04:42:28 -0600

    Update artists.php

--- artists.php
+++ artists.php
@@ -8,21 +8,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertArtist($_POST['aAName'], $_POST['aGenre'])) {
+      if (insertArtists($_POST['aAName'], $_POST['aGenre'])) {
         echo '<div class="alert alert-success" role="alert">Artist added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
   case "Edit":
-      if (updateArtist($_POST['aAName'], $_POST['aGenre'], $_POST['aid'])) {
+      if (updateArtists($_POST['aAName'], $_POST['aGenre'], $_POST['aid'])) {
         echo '<div class="alert alert-success" role="alert">Artist edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
   case "Delete":
-      if (deleteArtist($_POST['aid'])) {
+      if (deleteArtists($_POST['aid'])) {
         echo '<div class="alert alert-success" role="alert">Artist deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
